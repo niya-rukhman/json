d4869d2abdd78f026819cbda612b94fd1db60076
niya-rukhman <49750829+niya-rukhman@users.noreply.github.com>
2019-07-11 12:54:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let contact1 = {name : "Nida", 
+               "cell no" : "0345676879", 
+               "last message" : "Hello"}
+
+// let obj = JSON.parse(contact1);
+// document.getElementById("cont1").innerHTML = obj.name;
+         console.log(contact1);      
+               
+// let contact2 = {name : "Rukhman",
+//                 "cell no" : "0333785965",
+//                 "last message" : "Hello"}               
+            
+// let obj1 = JSON.parse(contact2);
+// document.getElementById("cont2").innerHTML = obj.name;
+                      
+
+
+// let contact3 ={name : "Haniya",
+//                "cell no" : "0333878768",
+//                "last message" : "Hello"}
+
+// let obj2 = JSON.parse(contact3);
+// document.getElementById("cont3").innerHTML = obj.name;
+                                            
